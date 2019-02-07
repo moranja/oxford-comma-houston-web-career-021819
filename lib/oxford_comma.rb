ce0505dc6bdd052e9588c8f58b602edc1ed0ea85
Adam Moran <jeffadammoran@gmail.com>
2019-02-07 22:59:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.size == 1
-    array.join
-    return array
+    new_string = array.join
+    return new_string
   elsif array.size == 2
-    array.join " and "
-    return array
+    new_string = array.join " and "
+    return new_string
   else
     last_entry = array.pop
     new_string = array.join(", ")
