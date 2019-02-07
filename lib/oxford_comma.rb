c68f065f6a46e938d176c9cd5bdb31e355e89b05
Adam Moran <jeffadammoran@gmail.com>
2019-02-07 22:59:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
   if array.size == 1
+    array.join
+    return array
+  elsif array.size == 2
+    array.join " and "
     return array
   else
     last_entry = array.pop
