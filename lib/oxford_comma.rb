cb3d5aed5abc795eac11531168424b7b30530426
Adam Moran <jeffadammoran@gmail.com>
2019-02-07 22:58:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   else
     last_entry = array.pop
     new_string = array.join(", ")
-    new_string << ", and"
+    new_string << ", and "
     new_string << "#{last_entry}"
     return new_string
   end
